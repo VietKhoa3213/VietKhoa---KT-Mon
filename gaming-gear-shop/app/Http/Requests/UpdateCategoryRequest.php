0cecd42058aa79acb0df8d036a1721729a5e54c6
VietKhoa3213 <168825671+VietKhoa3213@users.noreply.github.com>
2025-04-16 09:31:02 +0700

    Update UpdateCategoryRequest.php

--- gaming-gear-shop/app/Http/Requests/UpdateCategoryRequest.php
+++ gaming-gear-shop/app/Http/Requests/UpdateCategoryRequest.php
@@ -3,23 +3,16 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
-use Illuminate\Validation\Rule; // <<< Import Rule để dùng unique ignore
+use Illuminate\Validation\Rule;
 
 class UpdateCategoryRequest extends FormRequest
 {
-    /**
-     * Quyết định xem user có quyền thực hiện request này không.
-     */
     public function authorize(): bool
     {
-        return true; // Middleware đã check quyền
+        return true;
     }
 
-    /**
-     * Lấy các quy tắc validation áp dụng cho request.
-     *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
-     */
+  
     public function rules(): array
     {
 
