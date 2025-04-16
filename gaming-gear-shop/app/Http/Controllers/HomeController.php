9df2d68e883318e31eef56bcc40fe07d530f5eeb
VietKhoa3213 <168825671+VietKhoa3213@users.noreply.github.com>
2025-04-16 09:27:10 +0700

    Update HomeController.php

--- gaming-gear-shop/app/Http/Controllers/HomeController.php
+++ gaming-gear-shop/app/Http/Controllers/HomeController.php
@@ -19,7 +19,6 @@ class HomeController extends Controller
         $latestProducts = Product::with(['category', 'brand']) 
                                ->where('status', true)        
                                ->orderBy('created_at', 'desc') 
-                               // ->latest()
                                ->take(6)                      
                                ->get();                        
 
@@ -44,17 +43,15 @@ class HomeController extends Controller
                                 ->limit(5) 
                                 ->get();
         $discountedProducts = Product::with(['category', 'brand'])
-                                ->where('status', true)         // Đang bán
-                                ->whereNotNull('discount_price') // Có giá khuyến mãi
-                                ->where('discount_price', '>', 0) // Giá > 0
-                                ->whereColumn('discount_price', '<', 'price') // Giá KM < Giá gốc
-                                ->orderBy('updated_at', 'desc') // Ưu tiên SP mới cập nhật KM
-                                ->limit(6) // Lấy 8 sản phẩm KM
+                                ->where('status', true)         
+                                ->whereNotNull('discount_price') 
+                                ->where('discount_price', '>', 0) 
+                                ->whereColumn('discount_price', '<', 'price')
+                                ->orderBy('updated_at', 'desc')
+                                ->limit(6) 
                                 ->get();
         $brandsForMenu = Brand::orderBy('name', 'asc')
-                                // ->where('status', true) // If brands have status
-                                // ->where('is_featured', true) // If you want only featured brands
-                                ->limit(12) // Limit if needed
+                                ->limit(12) 
                                 ->get();
 
 
